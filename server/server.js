6ecb934a077306575f4a23132ed9b7a3aeafce36
Devin O'Leary <devin@Devins-MacBook-Pro.local>
2017-11-07 13:26:32 -0500

    added testing and GET route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,19 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});//by passing the res an object, you're allowing freedom to add other datatypes to the response.
+
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {
+  app
+};
---
+++ server/tests/server.test.js
@@ -0,0 +1,55 @@
+const request = require('supertest');
+const expect = require('expect')
+
+const {app} = require('../server');
+const {Todo} = require('../models/todo');
+
+
+beforeEach((done) => {
+  Todo.remove({}).then(() => done());
+}); //befor each test case, the Todo collection will be cleared out
+
+describe('Post /todos', () => { //describer block
+  it('should create a new todo', (done) => { //text to define test case. done is passed to end the async request
+    let text = 'Test todo text'; //variable that will be sent to server
+
+    request(app) // starts supertest using the app import from the express server
+    .post('/todos') //indicate posting action to the app's /todos route
+    .send({text}) // used by supertest to create an object to be sent to the server
+    .expect(200) //expecting an 'ok' code to return
+    .expect((res) => { //custom assertion that expects the response body to be equal to the text variable sent to it
+      expect(res.body.text).toBe(text);
+    })
+    .end((err, res) => { //ends the request creating an object that checks if there's an error or response and passes done to end the test case
+      if(err){
+        return done(err); //if error, call done with the error
+      }
+
+      Todo.find().then((todos) => { //if response, go into Todo collection and pass assertions
+        expect(todos.length).toBe(1); //make sure length is greater than 1
+        expect(todos[0].text).toBe(text); //make sure content is equal to var sent
+        done();//done
+      }).catch((err) => done(err));//if error, call done with the error
+
+    });
+  });
+
+  it('should not create todo with invalid body data', (done) => {//second test case, collection is cleared before this one starts
+
+    request(app)
+    .post('/todos')
+    .send({})//nothing is sent to the server
+    .expect(400)//model requires length of string to be greater than 0
+    .end((err,res) => {
+      if(err) {
+        return done(err);
+      }
+
+      Todo.find().then((todos) => {
+        expect(todos.length).toBe(0);
+        done();
+      }).catch((err) => done(err));
+    });
+  });
+
+});
